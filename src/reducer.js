3f25e9a98423ca7adc9cc5f4c0f18ec0920a8ad7
Saloni <salonimehta2127@gmail.com>
2022-01-05 01:08:25 -0500

    Completed

--- src/reducer.js
+++ src/reducer.js
@@ -1,1 +1,28 @@
 // add code snippets from README
+let container=document.getElementById("container")
+let state;
+
+function reducer(state={count:0},action){
+    switch(action.type){
+        case "counter/increment":
+            return {count: state.count+1}
+        default:
+            return state
+    }
+}
+
+function dispatch(action){
+    state=reducer(state,action)
+    render()
+}
+
+function render(){
+ let button=document.getElementById("button");
+ container.textContent=state.count
+
+}
+
+button.addEventListener("click",()=>{
+    dispatch({type:"counter/increment"})
+})
+dispatch({type:"@@INIT"})
